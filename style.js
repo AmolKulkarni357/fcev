3c799278239b7758d72239a3134a5d6cb3a2c548
AmolKulkarni357 <92637956+AmolKulkarni357@users.noreply.github.com>
2021-11-07 13:38:06 +0530

    Delete style.js

--- style.js
+++
@@ -1,7 +0,0 @@
-function show_part(event)
-{
-	event.preventDefault();
-	$("main>section").css("display","none");
-	$("#"+event.target.innerText.replaceAll(" ","_").replaceAll("-","_").replaceAll("/","_")).css("display","revert");
-}
-precision=4;
