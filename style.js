c2631c4efbd6ba1a1b1b5564075aeab8484d5ff0
AmolKulkarni357 <92637956+AmolKulkarni357@users.noreply.github.com>
2021-11-07 13:28:24 +0530

    Create style.js

---
+++ style.js
@@ -0,0 +1,7 @@
+function show_part(event)
+{
+	event.preventDefault();
+	$("main>section").css("display","none");
+	$("#"+event.target.innerText.replaceAll(" ","_").replaceAll("-","_").replaceAll("/","_")).css("display","revert");
+}
+precision=4;
